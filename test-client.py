0cc385c8000b4f66a02db41440b2985a654befa9
gastoniy <279533@student.pwr.edu.pl>
2026-05-28 19:35:27 +0200

    Add new client version based on OOP concepts

--- test-client.py
+++ test-client.py
@@ -1,32 +1,80 @@
 import socket
-import sys
-from os import environ
-from nacl.public import PrivateKey, PublicKey, Box
+import argparse
 import time
+import os
+import sys
+import yaml
+from pathlib import Path
+from nacl.public import PrivateKey, PublicKey, Box
 
-# if len(sys.argv) != ... # Finish while cryptography implementation
 
-server_ip = sys.argv[1]
-server_port = int(sys.argv[2])
-client_private_hex: str | None = environ.get("TEST_CLIENT_PRIVATE_KEY") # Getting as env var for tests purposes
-server_public_hex: str | None = environ.get("SERVER_PUBLIC_KEY") # In future will be given as args to script
+class SPAClient():
+    def __init__(self, config_path: str | None = None) -> None:
+        self.config: dict = self._load_config(config_path)
+        self.crypto_box: Box = self._load_crypto_box()
+    
+    def _load_config(self, config_path: str | None) -> dict:
+        """Load configuration from yaml file or returns empty dict"""
+        default_path = Path.home() / ".config" / "spa_client" / "config.yaml"
+        target_path = Path(config_path) if config_path else default_path
 
-if not client_private_hex or not server_public_hex:
-    raise ValueError("Missing PyNaCl key environment variables.")
+        if target_path.exists():
+            print(f"Using config: {target_path}")
+            with open(target_path, 'r') as f:
+                return yaml.safe_load(f) or {}
+        print("Config is not found! Trying env variables!")
+        return {}
+    
+    def _load_crypto_box(self) -> Box:
+        """Initialize crypto key from config OR env variables"""
+        # Priority: Env vars -> Config File
+        client_priv_hex = os.environ.get("SPA_CLIENT_PRIVATE_KEY") or self.config.get("client_private_key", None)
+        server_pub_hex = os.environ.get("SPA_SERVER_PUBLIC_KEY") or self.config.get("server_public_key", None)
 
-client_priv = PrivateKey(bytes.fromhex(client_private_hex))
-server_pub = PublicKey(bytes.fromhex(server_public_hex))
-crypto_box = Box(client_priv, server_pub)
-command = "Request for access via SPA"
+        if not client_priv_hex or not server_pub_hex:
+            raise ValueError("Cryptographic keys were not found! Check your config file or env variables!")
+        
+        try:
+            client_priv = PrivateKey(bytes.fromhex(client_priv_hex))
+            server_pub = PublicKey(bytes.fromhex(server_pub_hex))
+            return Box(client_priv, server_pub)
+        except ValueError as e:
+            raise ValueError(f"Wrong key format: {e}")
+        
+    def send_packet(self, server_ip: str, server_port: str, command: str = "Request Open via SPA") -> None:
+        """Encrypt and send SPA packet via UDP"""
+        timestamp = time.time()
+        payload = f"{command}|{timestamp}".encode("utf-8")
 
-timestamp = time.time()
-payload = f"{command}|{timestamp}".encode("utf-8")
+        try:
+            encrypted_token = self.crypto_box.encrypt(payload)
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
+                sock.sendto(encrypted_token, (server_ip, server_port))
+            print(f"Sent encrypted SPA token to {server_ip}:{server_port}")
+            print(f"Command sent: {command}")
+        except socket.error as e:
+            print(f"Network error while sending: {e}")
+        except Exception as e:
+            print(f"Unexpected Error: {e}", file=sys.stderr)
 
-try:
-    encrypted_token = crypto_box.encrypt(payload)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(encrypted_token, (server_ip, server_port))
+def main():
+    parser = argparse.ArgumentParser(description="Single Packet Authorization (SPA) Client")
+    parser.add_argument("server_ip", help="Destination Server IP")
+    parser.add_argument("-p", "--port", type=int, default=62201, help="UDP port of SPA Server (Default 62201)")
+    parser.add_argument("-c", "--command", type=str, default="Request Open via SPA", help="Command to process (Default 'Request Open via SPA')")
+    parser.add_argument("--config", type=str, help="Not standard path to configuration file")
 
-    print(f"SPA packet sent to {server_ip}:{server_port}")
-except Exception as e:
-    print(f"While sending an error occurred: {e}")
+    args = parser.parse_args()
+
+    try:
+        client = SPAClient(config_path=args.config)
+        client.send_packet(args.server_ip, args.port, args.command)
+    except ValueError as e:
+        print(f"Configuration Error: {e}", file=sys.stderr)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print(f"\nStopped by User", file=sys.stderr)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
